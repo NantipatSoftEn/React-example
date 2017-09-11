bd1f98bb9f96fdd2251d5257a0dcf5212f990f70
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-09-11 11:17:11 +0700

    bug

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import Footer from  './set/Footer';
+import Header from  './set/Header';
+import Content from  './set/Content';
+
 import App from './App';
+import {Router,Route,Link,browserHistory} from  'react-router'
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    // <App />,
+    // document.getElementById('root')
+    <Router history={browserHistory}>
+    <Route path = '/'component={App} />
+    <Route path = '/header'component={Header} />
+    <Route path = '/content'component={Content} />
+    <Route path = '/footer'component={Footer} />
+
+    </Router>,document.getElementById('root')
+);
 registerServiceWorker();
