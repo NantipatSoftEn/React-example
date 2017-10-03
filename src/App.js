8a906a7f91ee88ec5c25e6a15fc48b5808850f00
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-10-03 13:35:58 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
-import redux from './redux';
+//import redux from './redux';
+import ex from './redux-expensify';
 //import tredux from './testredux';
 class App extends Component {
 
   render() {
     return (
       <div>
-            <redux />
+      <ex />
       </div>
     );
   }
--- src/redux-expensify.js
+++ src/redux-expensify.js
@@ -1,7 +1,59 @@
 import { createStore, combineReducers} from 'redux';
 
+
+
+const addExpense = () => ({
+    
+})
+const expensesReducerDefaultState = [];
+
+const  expensesReducer = (state = [],action) => {
+    switch (action.type) {
+        default:
+        return state;
+
+    }
+};
+
+ // Filters Reducers
+ // text
+
+const filtersReducerDefaultState = {
+    text:'',
+    sortBy:'date',
+    startDate:undefined,
+    endDate:undefined
+};
+const filtersReducer = (state = filtersReducerDefaultState,action ) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+ // Store creation
+ const store = createStore(
+     combineReducers({
+         expenses:expensesReducer,
+         filters:filtersReducer
+     })
+ );
+
+ console.log(store.getState());
+
+
 const demoState ={
     expenses: [{
-        id:
-    }]
-}
+        id:'poijasdfhwer',
+        description:'Jabuary Rent',
+        note:'This was the final',
+        amount:54500,
+        createdAt:0
+    }],
+    filter:{
+        text:'rent',
+        sortBy:'amount',
+        startDate:undefined,
+        endDate:undefined
+    }
+};
