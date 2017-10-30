543d25d5c507898157c1619d484f30728c4e1249
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-10-30 14:09:42 +0700

    add midenware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,21 @@
-import {combineReducers , createStore} from 'redux';
+import {combineReducers , createStore,applyMiddleware} from 'redux';
 import todos from '../reducers/todos';
 import visibilityFilter from '../reducers/visibilityFilter';
 
+const mylogger = store => next => action =>{
+    console.log("Log Action",action);
+    next(action);
+}
+
+const middlewares = applyMiddleware(mylogger)
 export default () => {
   const store = createStore(
     combineReducers({
        todos,
        visibilityFilter
-    })
+   }),
+   {},
+   middlewares
   );
 
   return store;
