5232e9d09dab985082a8f3375ba5795e0ff9b796
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-11-01 14:17:52 +0700

    add Dashboard

--- src/containers/Dashbord.js
+++ src/containers/Dashbord.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const Item = () => (
     <div>
-        This is countItem =
+        Dashbord
     </div>
 );
 export default Item
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,13 +2,14 @@ import React from 'react';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodoList from '../containers/VisibleTodoList';
 import Footer from '../components/Footer';
-import Dashbord from '../components/Dashbord';
+import Dashbord from '../containers/Dashbord';
 const AppRouter = () => (
     <div>
-        <Dashbord />
+
         <AddTodo />
         <VisibleTodoList />
         <Footer />
+        <Dashbord />
     </div>
 );
 
