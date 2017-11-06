5ab3f1c440e232c52b7e4ad90c2bd1d1312e0fa1
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-11-06 10:20:07 +0700

    once data

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -9,40 +9,48 @@ const config = {
     messagingSenderId: "345142912527"
   };
 
- firebase.initializeApp(config);
+firebase.initializeApp(config);
 const database = firebase.database();
 
-database.ref().set({
-    class:'Novice',
-    lv:26,
-    jop:10,
-    location:{
-        city:'phuket',
-        country: 'United State'
-    }
-}).then( () => {
-    console.log("Data is save ");
-}).catch( (e) => {
-    console.log("This is failed.",e);
-});
-
-database.ref('lv').set(27);
-database.ref('attributes').set({
-    height:73,
-    weight:150
-}).then( () => {
-    console.log("Second set Call");
-}).catch( (e) => {
-    console.log("Things didnt for the Second");
-});
-
+// database.ref('attributes/height')
+// .once('value').
+// then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+// }).catch((e) => {
+//     console.log("Error",e);
+// })
+// database.ref().set({
+//     class:'Novice',
+//     lv:26,
+//     jop:10,
+//     location:{
+//         city:'phuket',
+//         country: 'United State'
+//     }
+// }).then( () => {
+//     console.log("Data is save ");
+// }).catch( (e) => {
+//     console.log("This is failed.",e);
+// });
+//
+// database.ref('lv').set(27);
+// database.ref('attributes').set({
+//     height:73,
+//     weight:150
+// }).then( () => {
+//     console.log("Second set Call");
+// }).catch( (e) => {
+//     console.log("Things didnt for the Second");
+// });
+//
 // database.ref('lv').remove().then(() => {
 //     console.log("data remove");
 // }).catch( (e) => {
 //     console.log("error");
 // })
-
-database.ref().update({
-    class:'Wizard',
-    jop:50
-});
+//
+// database.ref().update({
+//     class:'Wizard',
+//     jop:50
+// });
